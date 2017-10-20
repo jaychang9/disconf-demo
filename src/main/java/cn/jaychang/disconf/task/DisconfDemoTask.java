9764fe770c2089430faf6e629fc047855b61e939
zhangjie <zhangjie@choosefine.com>
2017-10-20 17:35:40 +0800

    新增一个DisconfDemoTask用于演示，当disconf-web修改配置后，能否及时收到配置更新后的内容

--- src/main/java/cn/jaychang/disconf/task/DisconfDemoTask.java
+++ src/main/java/cn/jaychang/disconf/task/DisconfDemoTask.java
@@ -12,6 +12,7 @@ package cn.jaychang.disconf.task;
 
 import cn.jaychang.disconf.component.RedisComponent;
 import cn.jaychang.disconf.config.RedisConfig;
+import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +25,7 @@ import org.springframework.stereotype.Service;
  * @version 2014-6-17
  */
 @Service
-public class DisconfDemoTask {
+public class DisconfDemoTask extends Thread{
 
     protected static final Logger LOGGER = LoggerFactory
         .getLogger(DisconfDemoTask.class);
@@ -37,27 +38,23 @@ public class DisconfDemoTask {
 
     private static final String REDIS_KEY = "disconf_key";
 
-    /**
-     *
-     */
-    public int run() {
+    @PostConstruct
+    public void init(){
+        this.start();
+    }
 
+    @Override
+    public void run() {
         try {
-
             while (true) {
-
                 Thread.sleep(5000);
-
+                //这里有个地方需要说明，修改disconf-web上的配置后，为什么还能获取redis里相应key的值，因为jedisPool的配置并没有更新过
+                //但我们能看到redis( 127.0.0.1,6379)  get key: hello disconf!  redis的主机名，端口号如果在disconf-web上修改后，打印的信息就会相应的修改
                 LOGGER.info("redis( " + redisConfig.getHost() + ","
-                    + redisConfig.getPort() + ")  get key: " + REDIS_KEY);
-
+                    + redisConfig.getPort() + ")  get key: " + simpleRedisService.get(REDIS_KEY));
             }
-
         } catch (Exception e) {
-
             LOGGER.error(e.toString(), e);
         }
-
-        return 0;
     }
 }
